c6a61de607568e2d65ce2c20a54a69f47bde13da
Alan Machado <alan.r.machado@outlook.com>
2019-03-19 22:17:55 -0300

    first

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
+const cors = require('cors');
+const pdfTemplate = require('./documents');
+
+
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.post('/create-pdf', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('rezultati.pdf', (err) => {
+      if(err) {
+          return console.log('error');
+      }
+  res.send(Promise.resolve())
+    });
+  })
+
+
+  app.get('/fetch-pdf', (req, res) => {
+    res.sendFile(`${__dirname}/rezultati.pdf`);
+  });
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
